5b414c0af844059df8a574e37a281f19b15bb071
Nicolas Hurtubise <316k@legtux.org>
2014-09-05 18:53:39 -0400

    Controller_Welcome => Controller_Index

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -140,6 +140,6 @@ Kohana::modules(array(
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'welcome',
+		'controller' => 'index',
 		'action'     => 'index',
 	));
