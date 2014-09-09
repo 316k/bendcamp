6c5e4c80920ab4578a945f0004f0dae037da482c
Nicolas Hurtubise <316k@legtux.org>
2014-09-09 00:43:21 -0400

    Restores the default route

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -138,7 +138,7 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('index', '')
+Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'index',
 		'action'     => 'index',
